1e187a95504857c32a0236508022760c180c752f
Sharma-sr7 <sharmasharmasrb@gmail.com>
2025-04-05 10:57:26 +0530

    Add Procfile and update requirements

--- Sr7Media/posts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post,LikePost,FollowersCount,Comment,DeletedUsers
-
-admin.site.register(Post)
-admin.site.register(LikePost)
-admin.site.register(FollowersCount)
-admin.site.register(Comment)
-admin.site.register(DeletedUsers)
--- Sr7Media/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-
-urlpatterns = [
-    path('',login_page, name='login_url'),
-    path('logout/',logout_page, name='logout_url'),
-    path('register/',register_page, name='register_url'),
-]
